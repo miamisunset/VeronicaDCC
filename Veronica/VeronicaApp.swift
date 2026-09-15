e19c03eaae05e501a6fd3060c60a7fdf093fde4e
Alex Yermolaev <ayermolaev@pm.me>
2026-09-15 11:15:31 -0400

    Initial Commit

---
+++ Veronica/VeronicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeronicaApp.swift
+//  Veronica
+//
+//  Created by Alex Yermolaev on 9/15/26.
+//
+
+import SwiftUI
+
+@main
+struct VeronicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
